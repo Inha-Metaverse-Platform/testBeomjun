8faddabb62cad13ba55becd1a600482eb7c5d7e9
0425kid <76833239+0425kid@users.noreply.github.com>
2022-10-05 17:06:15 +0900

    CRUD 게시판
    CRUD 게시판 기능 완성

--- server.js
+++ server.js
@@ -1,42 +1,164 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
+
+function templateHTML(title, list, body, control){
+  return `
+  <!doctype html>
+  <html>
+  <head>
+    <title>CRUD 게시판 - ${title}</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    <h1><a href="/">CRUD 게시판 연습</a></h1>
+    ${list}
+    ${control}
+    ${body}
+  </body>
+  </html>
+  `;
+}
+function templateList(filelist){
+  var list = '<ul>';
+  var i = 0;
+  while(i < filelist.length){
+    list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    i++;
+  }
+  list = list+'</ul>';
+  return list;
+}
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
-    var title = queryData.id;
+    var pathname = url.parse(_url, true).pathname;
+    if(pathname === '/'){
+      if(queryData.id === undefined){
+        fs.readdir('./data', function(error, filelist){
+          var title = '홈';
+          var description = '홈 화면입니다';
+          var list = templateList(filelist);
+          var template = templateHTML(title, list,
+            `<h2>${title}</h2>${description}`,
+            `<a href="/create">새 글 작성</a>`
+          );
+          response.writeHead(200);
+          response.end(template);
+        });
+      } else {
+        fs.readdir('./data', function(error, filelist){
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+            var title = queryData.id;
+            var list = templateList(filelist);
+            var template = templateHTML(title, list,
+              `<h2>${title}</h2>${description}`,
 
-
-
-    fs.readFile(`data/${title}`, 'utf-8', function(err, data){
-      var description = data;
-      var template = `
-      <!doctype html>
-      <html>
-      <head>
-        <title>${title}</title>
-        <meta charset="utf-8">
-      </head>
-      <body>
-        <h1><a href="/">HOME</a></h1>
-        <h3>Made by Jun</h3>
-        <ol>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ol>
-        <h2>${title}</h2>
-        <p>${description}
-        </p>
-      </body>
-      </html>
-      `;
-      response.writeHead(200);
-      response.end(template);
-    });
-
-
-
+              `<a href="/create">새 글 작성</a>
+               <a href="/update?id=${title}">글 수정</a>
+               <form action="delete_process" method="post">
+                 <input type="hidden" name="id" value="${title}">
+                 <input type="submit" value="delete">
+               </form>
+               `
+            );
+            response.writeHead(200);
+            response.end(template);
+          });
+        });
+      }
+    } else if(pathname === '/create'){
+      fs.readdir('./data', function(error, filelist){
+        var title = 'CRUD 게시판 - 새 글 작성';
+        var list = templateList(filelist);
+        var template = templateHTML(title, list, `
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="글 제목"></p>
+            <p>
+              <textarea name="description" rows="4" cols="50" placeholder="본문 내용"></textarea>
+            </p>
+            <p>
+              <input type="submit" placeholder="작성완료">
+            </p>
+          </form>
+        `, '');
+        response.writeHead(200);
+        response.end(template);
+      });
+    } else if(pathname === '/update') {
+      fs.readdir('./data', function(error, filelist){
+        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
+          var title = queryData.id;
+          var list = templateList(filelist);
+          var template = templateHTML(title, list,
+            `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="글 제목" value="${title}"></p>
+              <p>
+                <textarea name="description" rows="4" cols="50" placeholder="본문 내용">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit" placeholder="작성완료">
+              </p>
+            </form>
+            `,
+            `<a href="/create">새 글 작성</a>
+             <a href="/update?id=${title}">글 수정</a>`
+          );
+          response.writeHead(200);
+          response.end(template);
+        });
+      });
+    } else if(pathname === '/update_process'){
+      var body = '';
+      request.on('data', function(data){
+          body += data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          var title = post.title;
+          var description = post.description;
+          fs.rename(`data/${id}`, `data/${title}`, function(err){
+            fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+              response.writeHead(302, {Location: `/?id=${title}`});
+              response.end();
+          })
+        });
+      });
+    } else if(pathname === '/create_process'){
+      var body = '';
+      request.on('data', function(data){
+          body += data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var title = post.title;
+          var description = post.description;
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end();
+          })
+      });
+    } else if(pathname === '/delete_process'){
+      var body = '';
+      request.on('data', function(data){
+          body += data;
+      });
+      request.on('end', function(){
+          var post = qs.parse(body);
+          var id = post.id;
+          fs.unlink(`data/${id}`, function(err){
+            response.writeHead(302, {Location: `/`});
+            response.end();
+          })
+      });
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
+    }
 });
-app.listen(3000); //3000번 포트에 우리 node.js 서버를 올렸음
+app.listen(3000);
