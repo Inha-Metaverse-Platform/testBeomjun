d01f837addd9ba603255591648ef30b54640e776
0425kid <76833239+0425kid@users.noreply.github.com>
2022-10-26 22:57:00 +0900

    2022-10-26
    Unity 컨트롤 시도

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ var compression = require('compression');
 
 var template = require('./lib/template.js');
 
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var http = require('http').Server(app); //http 통신을 할거다
+var io = require('socket.io')(http); //http 통신 기반으로 socket 통신도 할거다
 
 app.use(bodyParser.urlencoded({extended: false })); //body-parser가 만들어내는 미들웨어를 표현하는 식?
 //기존에는 post 방식으로 연결했을 때, 콜백함수 내에서 body 변수를 따로 만들었다
@@ -46,46 +46,51 @@ var db = mysql.createConnection({ //conection을 생성한다
 db.connect();
 
 
-//postgres 연결 코드
-const {Pool} = require('pg');
-const pg = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'accounts',
-  password: 'password',
-  port: 5432 //postgres의 기본 포트인듯?
-})
+// //postgres 연결 코드
+// const {Pool} = require('pg');
+// const pg = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'accounts',
+//   password: 'password',
+//   port: 5432 //postgres의 기본 포트인듯?
+// })
+//
+// //postgres 잘 연결됐는지 확인
+// pg.connect(err => {
+//   if(err) console.log(err);
+//   else{
+//     console.log("postgres connected");
+//   }
+// })
+//
+// pg.query(`SELECT * FROM account`, (err, accounts) => {
+//   if(err) console.log(err);
+//   else {
+//     console.log(accounts);
+//   }
+// })
 
-//postgres 잘 연결됐는지 확인
-pg.connect(err => {
-  if(err) console.log(err);
-  else{
-    console.log("postgres connected");
-  }
-})
-
-pg.query(`SELECT * FROM account`, (err, accounts) => {
-  if(err) console.log(err);
-  else {
-    console.log(accounts);
-  }
-})
+//home화면
+// app.get('/', function(request, response) {
+//   db.query(`SELECT * FROM account`, function(err, accounts){
+//     var title = "INHA METAVERSE";
+//     var description = "Welcome to Inha Metaverse";
+//     var accountlist = template.list(accounts);
+//     // console.log("이건 mysql 버전");
+//     // console.log(accounts);
+//     var html = template.HTML(title, accountlist,
+//       `<h2>${title}</h2>${description}`,
+//       `<a href="/login">로그인</a>
+//       <a href="/create">회원가입</a>`
+//     );
+//     response.send(html);
+//   })
+// });
 
 app.get('/', function(request, response) {
-  db.query(`SELECT * FROM account`, function(err, accounts){
-    var title = "INHA METAVERSE";
-    var description = "Welcome to Inha Metaverse";
-    var accountlist = template.list(accounts);
-    console.log("이건 mysql 버전");
-    console.log(accounts);
-    var html = template.HTML(title, accountlist,
-      `<h2>${title}</h2>${description}`,
-      `<a href="/login">로그인</a>
-      <a href="/create">회원가입</a>`
-    );
-    response.send(html);
-  })
-});
+  response.sendFile(__dirname + "/home.html")
+})
 
 
 app.get('/account/:accountId', function(request, response){
@@ -211,6 +216,7 @@ app.post('/login_process', function(request, response){
             <h3>Hello, ${results[0].username}!</h3>
             <form action="/unity" method="post">
             <input type="submit" value="Go to Metaverse">
+            </form>
             `);
       } else {
         response.send('login failed');
@@ -395,6 +401,7 @@ if(currentUser)
 });//END_IO.ON
 });
 
+//process.env.port || 3000의 뜻은 process.env라는 객체에 port라는 설정이 있다면 그 속성을 사용하고, 없다면 3000을 사용한다는 뜻이다
 http.listen(process.env.PORT ||3000, function(){
 	console.log('listening on *:3000');
 });
---
+++ socket_practice.js
@@ -0,0 +1,25 @@
+// var express = require('express'); //express 불러옴
+// const app = express();
+
+var app = require("express")();
+var http = require('http').createServer(app); //http 통신을 위한 모듈
+var io = require('socket.io')(http); //socket 객체
+
+
+
+http.listen(3000, function(){ //3000 포트에서 socket 연결을 기다립니다
+  console.log("listening on *:3000");
+})
+
+//Client와의 연결 이벤트인 'connection'을 이벤트를 받는 on 함수로 받고
+io.on('connection', function (socekt) {
+  //연결되면 콘솔에 출력
+  console.log(socket.id, 'Connected');
+  //이건 클라이언트에게 전달..?
+  socket.emit('msg', `${socket.id} 연결 되었습니다.`);
+  socket.on('msg', function(data) {
+    console.log(socket.id, data);
+
+    socket.emit('msg', `Server : "${data}"받았습니다.`);
+  })
+})
